85f29118d6bddcca3cef76ee889b59ecd28bd30a
pahan <pahan33@users.sourceforge.net>
2017-02-10 13:30:36 +0300

    refactoring corresponding class renaming

--- src/main/java/com/github/ferstl/depgraph/graph/GraphBuilderAdapter.java
+++ src/main/java/com/github/ferstl/depgraph/graph/GraphBuilderAdapter.java
@@ -70,7 +70,7 @@ public final class GraphBuilderAdapter {
       throw new DependencyGraphException(e);
     }
 
-    DotBuildingVisitor visitor = new DotBuildingVisitor(dotBuilder, this.targetFilter);
+    GraphBuildingVisitor visitor = new GraphBuildingVisitor(dotBuilder, this.targetFilter);
     root.accept(visitor);
   }
 
@@ -83,7 +83,7 @@ public final class GraphBuilderAdapter {
     }
 
     // Due to MNG-3236, we need to filter the artifacts on our own.
-    DotBuildingVisitor visitor = new DotBuildingVisitor(dotBuilder, globalFilter, this.targetFilter, this.includedResolutions);
+    GraphBuildingVisitor visitor = new GraphBuildingVisitor(dotBuilder, globalFilter, this.targetFilter, this.includedResolutions);
     root.accept(visitor);
   }
 }
